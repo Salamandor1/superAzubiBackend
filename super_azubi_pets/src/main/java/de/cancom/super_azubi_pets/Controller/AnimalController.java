5f8faffd71e8a3e03530d00419326151757a9ce6
Matthias <matthias.luetgemeier@cancom.de>
2025-06-27 11:02:20 +0000

    Add some functions to AnimalController (Edit, ceate, delete...)

--- super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Controller/AnimalController.java
+++ super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Controller/AnimalController.java
@@ -1,6 +1,5 @@
 package de.cancom.super_azubi_pets.Controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +16,28 @@ public class AnimalController {
 
     @Autowired
     AnimalRepository repository;
-    
+
     public ResponseEntity<Animal> getAinmal(@PathVariable Long id) {
         return new ResponseEntity<Animal>(repository.findById(id)
                 .orElse(null), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Animal> createAnimal(Animal animal) {
+        Animal savedAnimal = repository.save(animal);
+        return new ResponseEntity<Animal>(savedAnimal, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Void> deleteAnimal(@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+
+    public ResponseEntity<Animal> updateAnimal(@PathVariable Long id, Animal animal) {
+        if (!repository.existsById(id)) {
+            return new ResponseEntity<Animal>(HttpStatus.NOT_FOUND);
+        }
+        animal.setAnimalId(id);
+        Animal updatedAnimal = repository.save(animal);
+        return new ResponseEntity<Animal>(updatedAnimal, HttpStatus.OK);
     }
 }
