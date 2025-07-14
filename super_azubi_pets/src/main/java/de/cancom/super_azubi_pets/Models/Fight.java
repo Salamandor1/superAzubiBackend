730cc7939a1f3fcf26e569be9e867f1b6d443baa
joshua.wertgen <joshua.wertgen@hotmail.com>
2025-07-14 12:37:22 +0000

    Issue #34: refactor: moved class fields from service class to model class for clearer responsibilites and to prevent thread issues // reworked the service class accordingly.

--- super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Models/Fight.java
+++ super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Models/Fight.java
@@ -1,12 +1,23 @@
 package de.cancom.super_azubi_pets.Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fight {
+
+    private Game game;
 
     private Team playerTeam;
     private Team npcTeam;
-    private Log log;
+
+    private List<TeamAnimal> playerTeamAnimals = new ArrayList<>();
+    private List<TeamAnimal> enemyTeamAnimals = new ArrayList<>();
 
     public Fight() {
+    }
+
+    public Fight(Game game) {
+        setGame(game);
     }
 
     public Fight(Team playerTeam, Team npcTeam) {
@@ -22,8 +33,24 @@ public class Fight {
         this.npcTeam = npcTeam;
     }
 
-    public void setLog(Log log) {
-        this.log = log;
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void setPlayerTeamAnimals(List<TeamAnimal> playerTeamAnimals) {
+        this.playerTeamAnimals = playerTeamAnimals;
+    }
+
+    public void setEnemyTeamAnimals(List<TeamAnimal> enemyTeamAnimals) {
+        this.enemyTeamAnimals = enemyTeamAnimals;
+    }
+
+    public void replacePlayerTeamAnimal(int index, TeamAnimal newAnimal) {
+        playerTeamAnimals.set(index, newAnimal);
+    }
+
+    public void replaceEnemyTeamAnimal(int index, TeamAnimal newAnimal) {
+        enemyTeamAnimals.set(index, newAnimal);
     }
 
     public Team getPlayerTeam() {
@@ -34,8 +61,16 @@ public class Fight {
         return npcTeam;
     }
 
-    public Log getLog() {
-        return log;
+    public List<TeamAnimal> getPlayerTeamAnimals() {
+        return playerTeamAnimals;
+    }
+
+    public List<TeamAnimal> getEnemyTeamAnimals() {
+        return enemyTeamAnimals;
+    }
+
+    public Game getGame() {
+        return game;
     }
 
 }
