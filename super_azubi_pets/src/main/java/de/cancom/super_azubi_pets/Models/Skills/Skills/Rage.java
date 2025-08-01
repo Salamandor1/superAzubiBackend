c6029bb0a9e21e20e30cc2ddfa716d7ddf33564c
joshua.wertgen <joshua.wertgen@hotmail.com>
2025-08-01 09:53:52 +0000

    tweak: tweaked rage skill

--- super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Models/Skills/Skills/Rage.java
+++ super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Models/Skills/Skills/Rage.java
@@ -7,14 +7,10 @@ import de.cancom.super_azubi_pets.Models.Skills.Trigger;
 
 public class Rage implements Skill {
 
-    int hits = 0;
-    private final double boost;
+    private final int boost;
 
     public Rage(int level, int tier) {
-        this.boost = 0.10 + (level / 100.0) + (tier / 10.0);
-        if (level >= 10 || tier >= 5) {
-            hits++;
-        }
+        this.boost = (int) (1 + (Math.round(level / 10.0)) + (Math.round(tier / 2.0)));
     }
 
     @Override
@@ -29,16 +25,12 @@ public class Rage implements Skill {
 
     @Override
     public String getDescription() {
-        return "Erhöht nach drei Treffern die Angriffskraft um " + Math.round(boost * 100) + "%.";
+        return "Erhöht nach jedem Treffern die Angriffskraft um " + boost + ".";
     }
 
     @Override
     public void apply(FightState state, String source) {
-        hits++;
-        if (hits < 3) {
-            return;
-        }
-        hits = 0;
+
         TeamAnimal target;
         if (source.equals("player")) {
             target = state.getPlayerTeam().get(0);
@@ -48,13 +40,13 @@ public class Rage implements Skill {
         if (target.getHealth() <= 0) {
             return;
         }
-        target.setAttack((int) Math.round(target.getAttack() * (1.0 + boost)));
+        target.setAttack(target.getAttack() + boost);
         if (source.equals("player")) {
             state.setLog(state.getLog() + "[RAGE] (" + target.getEmoji() + ", Spieler) - Angriff wurde um "
-                    + Math.round(target.getAttack() * (1.0 + boost)) + " erhöht.\n");
+                    + boost + " erhöht.\n");
         } else {
             state.setLog(state.getLog() + "[RAGE] (" + target.getEmoji() + ", Gegner) - Angriff wurde um "
-                    + Math.round(target.getAttack() * (1.0 + boost)) + " erhöht.\n");
+                    + boost + " erhöht.\n");
         }
     }
 }
