f375629d95000c982aeee65b7007f0710409ebeb
Salamandor1 <122486091+Salamandor1@users.noreply.github.com>
2025-08-07 14:25:36 +0200

    add corscongif

--- super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/CorsConfig.java
+++ super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://superfrontend.onrender.com") // URL deines Frontends bei Render
+                    .allowedOrigins("http://localhost:4200", "https://superfrontend.onrender.com") // URL deines Frontends bei Render
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*");
             }
