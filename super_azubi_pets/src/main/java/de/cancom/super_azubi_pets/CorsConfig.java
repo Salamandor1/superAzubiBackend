0db10b9cf9d4c4bd86265cacb925a6916a434803
Salamandor1 <122486091+Salamandor1@users.noreply.github.com>
2025-08-07 14:23:57 +0200

    add corscongif

--- super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/CorsConfig.java
+++ super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/CorsConfig.java
@@ -1,3 +1,5 @@
+package de.cancom.super_azubi_pets;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -5,16 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Erlaubt CORS für alle Pfade
-                    .allowedOrigins("https://super-azubi-backend.onrender.com/") // URL deines Frontends auf Render
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Erlaubte HTTP Methoden
-                    .allowedHeaders("*"); // Erlaubt alle Header
+                registry.addMapping("/**")
+                    .allowedOrigins("https://superfrontend.onrender.com") // URL deines Frontends bei Render
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
             }
         };
     }
