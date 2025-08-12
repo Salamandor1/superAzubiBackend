f79838d9e20e8ba9d11501c911774874ff24cf64
Matthias <matthias.luetgemeier@cancom.de>
2025-08-12 11:21:48 +0200

    try to fix a problem with courage.java

--- super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Models/Skills/Skills/Courage.java
+++ super_azubi_pets/src/main/java/de/cancom/super_azubi_pets/Models/Skills/Skills/Courage.java
@@ -6,32 +6,31 @@ import de.cancom.super_azubi_pets.Models.Skills.Skill;
 import de.cancom.super_azubi_pets.Models.Skills.Trigger;
 
 public class Courage implements Skill {
-
+    
     private final int tier;
-
+    
     public Courage(int level, int tier) {
         this.tier = tier;
     }
-
+    
     @Override
     public String getName() {
         return "Mut";
     }
-
+    
     @Override
     public String getDescription() {
         return "Erhöht den Angriff aller tiere um " + tier;
     }
-
+    
     @Override
     public Trigger getTrigger() {
         return Trigger.ON_GAME_START;
     }
-
+    
     @Override
-<<<<<<< HEAD
     public void apply(FightState state, String source, TeamAnimal user) {
-
+        
         if (source.equals("player")) {
             source = "Spieler";
             for (TeamAnimal animal : state.getPlayerTeam()) {
@@ -44,32 +43,7 @@ public class Courage implements Skill {
             }
         }
         state.setLog(state.getLog() + "[MUT](" + user.getEmoji() + ", " + source
-                + ") - erhöht den Angriff aller Teammitglieder um " + tier + ".\n");
-=======
-    public void apply(FightState state, String source) {
-
-        String emoji = "";
-
-        if (source.equals("player")) {
-            for (TeamAnimal animal : state.getPlayerTeam()) {
-                animal.setAttack(animal.getAttack() + tier);
-                if (animal.getSkill() == this && emoji.equals("")) {
-                    emoji += animal.getEmoji();
-                    animal.setSkill(new None(animal.getLevel(), animal.getTier()));
-                }
-            }
-            state.setLog(state.getLog() + "[MUT] (" + emoji + ", Spieler) - ");
-        } else {
-            for (TeamAnimal animal : state.getEnemyTeam()) {
-                animal.setAttack(animal.getAttack() + tier);
-                if (animal.getSkill() == this && emoji.equals("")) {
-                    emoji += animal.getEmoji();
-                    animal.setSkill(new None(animal.getLevel(), animal.getTier()));
-                }
-            }
-            state.setLog(state.getLog() + "[MUT] (" + emoji + ", Gegner) - ");
-        }
-        state.setLog(state.getLog() + "erhöht den Angriff aller Teammitglieder um " + tier + ".\n");
->>>>>>> feat_online
+        + ") - erhöht den Angriff aller Teammitglieder um " + tier + ".\n");
     }
 }
+
